43deafecaab6cabb118fa7d2c07781db4f2d9a4c
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-28 14:50:56 -0500

    corrected run, working

--- lib/changer.rb
+++ lib/changer.rb
@@ -1,59 +1,23 @@
 class Changer
 
-  attr_accessor :coin_values, :index
-
-  def self.run(amount)
+  def run(amount)
     coin_values = [25, 10, 5, 1]
     return_coins = []
-    amount_left = amount
-    index = 0
-    coin_to_divide_by = coin_values[index]
 
-    while amount_left > 0
-      if amount >= coin_to_divide_by
-        num_coins = amount/coin_values[index]
-        return_coins.push([coin_values[index]] * num_coins)
-        amount_left = amount % coin_to_divide_by
-        coin_to_divide_by = coin_values[index + 1]
-      else
-        coin_to_divide_by = coin_values[index + 1]
-        #coin_to_divide_by = 10 #amount_left still = amount
+    while amount > 0
+        coin_values.each do |coin|
+      if amount >= coin
+        num_coins = amount/coin
+        num_coins.times do
+        return_coins << coin
+        end
+        amount = amount - (coin * num_coins)
       end
-      amount_left = 0
     end
   end
+  print return_coins
+end
 end
 
-#break loop when amount_left = 0
-# coin_values.cycle do |coin|
-#     number_into = amt / coin
-#      if number_into >= 1
-#       return_coins << [coin] * number_into
-#       amount_left = amt % coin
-
-
-
-    # elsif amt 24 > coin_values[0] 25
-    #   amount_left = amt % coin_values[0]
-    #   num_quarters = amount_left / coin_values[0]
-    #   return_coins << coin_values[0] * num_quarters
-    # else
-    #   if amount_left > coin_values[1]
-    #     amount_left = amount_left % coin_values[1]
-    #     num_dimes = amount_left / coin_values[1]
-    #     return_coins << coin_values[1] * num_dimes
-
-    #   elsif amount_left > coin_values[2]
-    #     amount_left = amount_left % coin_values[2]
-    #     num_nickels = amount_left / coin_values[2]
-    #     return_coins << coin_values[1] * num_nickels
-
-    #   elsif amount_left > coin_values[3]
-    #     amount_left = amount_left % coin_values[3]
-    #     #amount_left = 0
-    #     num_pennies = amount_left / coin_values[3]
-    #     return_coins << coin_values[3] * num_pennies
-
-    #   coins_returned.push(amt / coin_values[1]) #flatten
-    #   end
-
+changer = Changer.new
+changer.run(90)
